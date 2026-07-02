dfb619b631c595c5c1017eb79612f50948978dcc
Leonardo Toffolin <ltoffoli@lxplus977.cern.ch>
2026-07-02 17:42:56 +0200

    Coloured message

--- init_dirac_proxy_prod.sh
+++ init_dirac_proxy_prod.sh
@@ -3,7 +3,7 @@ echo "Initializing the DIRAC/Grid proxy ..."
 dirac-proxy-init -g fcc_prod
 if test "x$?" = "x0" ; then
    echo "Done!"
-   echo "You're running with fcc_prod permissions"
+   echo -e "\033[94mPlease note you are running with fcc_prod permissions\033[0m"
 else
    echo "Some problem occured ..."
 fi
